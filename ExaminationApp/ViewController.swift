6919715107a798c3b0e6c17ad87140f8f33b96b8
Student <student@m303-23.local>
2016-07-23 12:06:24 +0900

    exam sy

--- ExaminationApp/ViewController.swift
+++ ExaminationApp/ViewController.swift
@@ -13,24 +13,40 @@ class ViewController: UIViewController {
     private let operatorList = [
         "+",
         "-",
+        "×",
+        "÷"
     ]
     
     /// 選択中の演算子
-    private var selectedOperator = '+'
+    private var selectedOperator = "+"
     
     /// 値1の入力フィールド
     @IBOutlet private var value1: UITextField!
     /// 値2の入力フィールド
     @IBOutlet private var value2: UITextField!
     
+    @IBOutlet weak var labelResult: UILabel!
+
+    
     /// 計算実行ボタン押下時の処理
     @IBAction private func calcurate(_: UIButton) {
-        let result = Int(value1.text!)! + Int(value2.text)
+        var result = 0
+        if(selectedOperator == "+"){
+            result = Int(value1.text!)! + Int(value2.text!)!
+        }else if(selectedOperator == "-"){
+            result = Int(value1.text!)! - Int(value2.text!)!
+        }else if(selectedOperator == "×"){
+            result = Int(value1.text!)! * Int(value2.text!)!
+        }else if(selectedOperator == "÷"){
+            result = Int(value1.text!)! / Int(value2.text!)!
+        }
+        
         
         // TODO: 計算結果ラベルの値を書き換えるようにする
-        print("result: \(result)")
+        labelResult.text = "result: \(result)"
     }
-}
+    
+    }
 
 extension ViewController: UIPickerViewDataSource {
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -40,7 +56,7 @@ extension ViewController: UIPickerViewDataSource {
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         // コンポーネント毎の行数を返す
-        return 2
+        return operatorList.count
     }
 }
 
@@ -53,5 +69,6 @@ extension ViewController: UIPickerViewDelegate {
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         // 行を選択した時のアクションを定義
         // TODO: 選択した演算子で selectedOperator を上書きする
+        selectedOperator = operatorList[row]
     }
 }
